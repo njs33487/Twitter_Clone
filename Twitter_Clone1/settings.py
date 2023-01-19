e93542383ccc8669c48c3e458daec3e2b81234d2
njs33487 <njs33487@outlook.com>
2023-01-19 13:22:22 -0500

    update 20230119

--- Twitter_Clone1/settings.py
+++ Twitter_Clone1/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
